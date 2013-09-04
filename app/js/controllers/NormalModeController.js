ce9ac02293e503eef2b9f50fea272ae3a6ca9bd2
masimo <shok.max@gmail.com>
2013-09-04 18:10:57 +0300

    first commit

---
+++ app/js/controllers/NormalModeController.js
@@ -0,0 +1,124 @@
+'use strict';
+
+
+demoTask.controller('NormalModeController', function NormalModeController($scope, $http, $filter) {
+
+	$scope.pagina = 20;
+	$scope.paginationBut = 7;
+	$scope.activePage = 0;
+
+	var records = $scope.records;
+	var filter = $scope.filter = {};
+
+
+	$scope.$watch('filter', filterAndSortData, true);
+
+
+	$scope.$watch('records', function() {
+
+		if ($scope.records === 0) {
+			$scope.pages = 0;
+			return false
+		}
+
+
+		$scope.pages = Math.ceil($scope.records / $scope.pagina) - 1;
+		$scope.activePage = $scope.activePage > $scope.pages ? $scope.pages : $scope.activePage;
+
+	});
+
+
+	function filterAndSortData() {
+
+		
+
+		// sort
+		$scope.userData.sort(function(a, b) {
+			if (a[filter.sortBy] > b[filter.sortBy]) {
+				return filter.sortAsc ? 1 : -1;
+			}
+
+			if (a[filter.sortBy] < b[filter.sortBy]) {
+				return filter.sortAsc ? -1 : 1;
+			}
+
+			return 0;
+		});
+
+		// sort by numbers
+		$scope.userData.sort(function(a, b) {
+			if (a[filter.sortBy] > b[filter.sortBy]) {
+				return b[filter.sortBy] - a[filter.sortBy];
+			}
+
+			if (a[filter.sortBy] < b[filter.sortBy]) {
+				return a[filter.sortBy] - b[filter.sortBy];
+			}
+
+		});
+
+		
+	};
+
+	$scope.sortBy = function(key) {
+		if (filter.sortBy === key) {
+			filter.sortAsc = !filter.sortAsc;
+		} else {
+			filter.sortBy = key;
+			filter.sortAsc = true;
+		}
+	};
+
+	$scope.sortIconFor = function(key) {
+		if (filter.sortBy !== key) {
+			return '';
+		}
+
+		return filter.sortAsc ? '\u25B2' : '\u25BC';
+	};
+
+
+
+	$scope.init = function() {
+		
+		$scope.activePage = 0;
+		$scope.userData = [];
+
+		$http({
+			method: 'GET',
+			url: 'data/data.json'
+		}).
+		success(function(data, status, headers, config) {
+
+			angular.forEach(data, function(value, key) {
+
+				if (!key) return;
+
+				$scope.userData.push({
+					id: value[0],
+					name: value[1],
+					price: value[2],
+					bid: value[3]
+				});
+
+			});
+
+
+			$scope.pages = Math.ceil($scope.userData.length / $scope.pagina) - 1;
+
+
+		}).
+		error(function(data, status, headers, config) {
+			console.log('crashed');
+		});
+		
+
+	}
+
+
+
+	$scope.init();
+
+
+
+});
